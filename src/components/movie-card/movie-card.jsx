2fd996c2a8771685571b85015507c0275ed679f7
Minhaj Islam <minhajislam674@gmail.com>
2022-11-18 11:15:02 +0100

    Applied react-bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
